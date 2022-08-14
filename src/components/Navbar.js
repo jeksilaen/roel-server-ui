47c5f5980db9b011f76977206e52677894ad5515
Kevin Febrian <pownyfals@gmail.com>
2022-08-14 18:22:59 +0700

    add files

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='nav bg-slate-800 text-white flex justify-between items-center p-4 shadow-xl'>
+        <Link to="/" className='text-2xl font-medium font-minecraft'>myBook</Link>
+        <ul className='p-0 m-0 flex text-xl items-stretch'>
+            <CustomLink to="/about">About</CustomLink>
+            <CustomLink to="/pricing">Pricing</CustomLink>
+        </ul>
+    </nav>
+  )
+}
+
+function CustomLink({to, children, ...props}){
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path:resolvedPath.pathname, end:true})
+    return(
+        <li className={isActive ? "bg-slate-400 px-5 mx-3 rounded-md" : "px-5 mx-3 hover:bg-slate-600 rounded-md"}>
+            <Link to={to} {...props} className='flex items-center p-1 font-minecraft' >
+                {children}
+            </Link>
+        </li>
+    )
+}
+
+export default Navbar

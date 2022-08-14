c8ba069de0c19998dc85c48bbcdfc81f97f1a37b
Kevin Febrian <pownyfals@gmail.com>
2022-08-14 18:53:37 +0700

    add card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='bg-slate-500 w-screen h-2'>
-        
+    <div className=' h-2'>
+        <div className='flex justify-evenly bg-slate-700 h-96'>
+            <div className='border w-36 h-40'></div>
+            <div className='border w-36 h-40'></div>
+        </div>
     </div>
   )
 }

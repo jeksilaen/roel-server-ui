cde93ecc0d16e8ab9439b001bd75c793474c03eb
Kevin Febrian <pownyfals@gmail.com>
2022-08-18 22:08:34 +0700

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const About = () => {
-  return (
-    <div>
-        <h1>About</h1>
-    </div>
-  )
-}
+const About = () => (
+  <div className=' overflow-hidden'>
+    {/* <div className='w-screen h-screen bg-[url("./img/mc.gif")]'>
+        </div> */}
+    <div class="max-w-xl mx-auto h-96 bg-slate-300 opacity-80" style={{ backgroundImage : "url(./img/1.jpg)" }}></div>
+  </div>
+
+)
 
 export default About
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       fontFamily:{
         'retroGaming':['RetroGaming','serif'],
         'minecraft':['Minecraft','serif']
+      },
+      backgroundImage:{
+        'minecraft':"url(./img/mc.gif')"
       }
     },
   },

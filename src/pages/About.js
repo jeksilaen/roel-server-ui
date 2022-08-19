a33a51722066232a14c30844ee4396afbb283a51
Pownyfals <pownyfals@gmail.com>
2022-08-19 13:59:05 +0700

    about background

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import background from "../img/mc.gif";
 
 const About = () => (
   <div className=' overflow-hidden'>
     {/* <div className='w-screen h-screen bg-[url("./img/mc.gif")]'>
         </div> */}
-    <div class="max-w-xl mx-auto h-96 bg-slate-300 opacity-80" style={{ backgroundImage : "url(./img/1.jpg)" }}></div>
-  </div>
+    <div class="w-screen h-screen bg-slate-600 bg-blend-multiply opacity-80" style={{ backgroundImage : `url(${background})` }}></div>
+     
+    </div>
 
 )
 

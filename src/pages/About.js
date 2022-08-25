93109af6e23bb60b8eba389ba5d8e31ba58dfc7c
Pownyfals <pownyfals@gmail.com>
2022-08-25 12:54:38 +0700

    edit about js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,12 @@ import background from "../img/mc.gif";
 
 const About = () => (
   <div className=' overflow-hidden'>
-    {/* <div className='w-screen h-screen bg-[url("./img/mc.gif")]'>
-        </div> */}
-    <div class="w-screen h-screen bg-slate-600 bg-blend-multiply opacity-80" style={{ backgroundImage : `url(${background})` }}></div>
+    <div class="w-screen h-screen bg-slate-700 bg-blend-multiply opacity-80" style={{ backgroundImage : `url(${background})` }}>\
+          <div>
+          <h1 className='font-retroGaming text-center text-white text-3xl py-32'>UwuRoelTech</h1>
+          <p></p>
+          </div>
+    </div>  
      
     </div>
 

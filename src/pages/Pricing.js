0365eca55956dd46d7f76722cfa41f9d06d93c90
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2022-11-02 19:56:38 +0700

    change card color and bg color

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Pricing = () => {
   return (
-    <div className="bg-slate-700 h-screen">
-      <div className="flex flex-col h-[85%] items-center px-16 py-16 space-y-16 space-x-8 md:flex-row">
+    <div>
+      <div className="flex flex-col h-[85vh] items-center px-16 py-16 space-y-16 space-x-8 md:flex-row bg-slate-900">
 
         {/* Heading */}
-        <div className="flex flex-col w-1/3 space-y-5 pr-6">
+        <div className="flex flex-col w-1/3 space-y-5 pr-6 text-center md:text-left">
           <h1 className='text-white text-5xl font-semibold font-retroGaming'>Let's Start Now!</h1>
           <p className='text-white text-xl font-extralight'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo aspernatur recusandae exercitationem nobis, fugit, deleniti doloribus suscipit soluta architecto optio dicta voluptatum, sunt voluptas vel. Asperiores voluptates voluptatem enim!"</p>
         </div>
@@ -14,7 +14,7 @@ const Pricing = () => {
         {/* Cards */}
         <div className="flex flex-col w-2/3 h-full space-x-5 md:flex-row md:items-end">
           {/* Card 1 */}
-          <div className="flex flex-col py-5 drop-shadow-md bg-violet-600 text-white h-[87%] w-1/3 rounded-xl justify-between">
+          <div className="flex flex-col py-5 drop-shadow-md bg-slate-500 text-white h-[87%] w-1/3 rounded-xl justify-between">
             {/* Content */}
             <div className='space-y-4 text-center'>
               <p>Basic</p>
@@ -28,7 +28,7 @@ const Pricing = () => {
             </div>
             
             {/* CTA */}
-            <button className='bg-white font-bold text-violet-600 w-[60%] mx-auto h-[40px] rounded-xl hover:bg-slate-600 hover:text-white'>Get Started</button>
+            <button className='bg-white font-bold text-slate-500 w-[60%] mx-auto h-[40px] rounded-xl hover:bg-slate-600 hover:text-white'>Get Started</button>
           </div>
 
           {/* Card 2 */}
@@ -75,7 +75,7 @@ const Pricing = () => {
 
       <hr className="w-[88%] mx-auto my-0" />
 
-      <div className="flex flex-col-reverse justify-between bg-slate-700 md:flex-row mx-0 items-center h-[16%] px-[4rem] text-white">
+      <div className="flex flex-col-reverse justify-between bg-slate-900 md:flex-row mx-0 items-center h-[10vh] px-[4rem] text-white pb-1">
         {/* Logo */}
         <div className="flex justify-around space-x-3 items-center">
           <h4 className='text-3xl font-semibold'>ROEL</h4>
@@ -87,9 +87,9 @@ const Pricing = () => {
 
         {/* Socials */}
         <div className="flex justify-around items-center w-[25%] font-semibold">
-          <p className='text-6lg text-bold hover:cursor-pointer'>Youtube</p>
-          <p className='text-6lg text-bold hover:cursor-pointer'>Instagram</p>
-          <p className='text-6lg text-bold hover:cursor-pointer'>LinkedIn</p>
+          <p className='text-6lg text-bold hover:cursor-pointer hover:text-slate-300'>Youtube</p>
+          <p className='text-6lg text-bold hover:cursor-pointer hover:text-slate-300'>Instagram</p>
+          <p className='text-6lg text-bold hover:cursor-pointer hover:text-slate-300'>LinkedIn</p>
         </div>
       </div>
 
